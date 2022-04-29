c50b513bf4a71bb747cafbf32004ba4f1691deb2
danielhillsman <daniel.loves.junk@gmail.com>
2022-04-29 02:41:57 -0700

    added comments

--- Assets/assets/js/script.js
+++ Assets/assets/js/script.js
@@ -1,46 +1,53 @@
+// Created Vars from a document.querySelector
 const userInput = $('#userInput')[0];
 const submitBtn = $('#submitBtn')[0];
 const searchHistoryDiv = $('#searchHistory')[0];
+// Search bar vars
 let cityResults = $('#cityResults')[0];
 let currentCity = '';
 let searchHistory = [];
 let cityName = $('#cityName')[0];
+// Search bar vars
 let mainDate = $('#mainDate')[0];
 let mainIcon = $('#mainIcon')[0];
 let mainTemp = $('#mainTemp')[0];
 let mainHumid = $('#mainHumid')[0];
 let mainWind = $('#mainWind')[0];
 let mainUv = $('#mainUv')[0];
+// Day 1 vars
 let day1Temp = $('#day1Temp')[0];
 let day1Date = $('#day1Date')[0];
 let day1Icon = $('#day1Icon')[0];
 let day1Wind = $('#day1Wind')[0];
 let day1Humid = $('#day1Humid')[0];
+// Day 1 vars
 let day2Temp = $('#day2Temp')[0];
 let day2Date = $('#day2Date')[0];
 let day2Icon = $('#day2Icon')[0];
 let day2Wind = $('#day2Wind')[0];
 let day2Humid = $('#day2Humid')[0];
+// Day 1 vars
 let day3Temp = $('#day3Temp')[0];
 let day3Date = $('#day3Date')[0];
 let day3Icon = $('#day3Icon')[0];
 let day3Wind = $('#day3Wind')[0];
 let day3Humid = $('#day3Humid')[0];
+// Day 1 vars
 let day4Temp = $('#day4Temp')[0];
 let day4Date = $('#day4Date')[0];
 let day4Icon = $('#day4Icon')[0];
 let day4Wind = $('#day4Wind')[0];
 let day4Humid = $('#day4Humid')[0];
+// Day 1 vars
 let day5Temp = $('#day5Temp')[0];
 let day5Date = $('#day5Date')[0];
 let day5Icon = $('#day5Icon')[0];
 let day5Wind = $('#day5Wind')[0];
 let day5Humid = $('#day5Humid')[0];
 
-
-
+//==============================//
 // -----Search Bar Function-----//
-
+//=============================//
 // Saves Search to local storage 
 let saveCity = (nextCity) => {
   let cities = JSON.parse(localStorage.getItem("results"));
@@ -80,9 +87,12 @@ let cityFunction = () => {
 } // returns History when page is rereshed
 cityFunction()
 //-----End Of Search Bar Function-----//
+//====================================//
 
 // API Key
 const apiKey = "9e2e3f847b172f233503288a97eb4446";
+
+//========================//
 //----Weather Function----//
 let weatherFunction = (searchCity) => {
   // Calling API Key
@@ -114,7 +124,6 @@ let weatherFunction = (searchCity) => {
         } else if (oneCallData.current.uvi >= 8) {
           mainUv.classList.add('uvRed');
         }
-        
           // 1st Next Day
           $(day1Date).text(moment().add(1, 'day').format("MM/DD/YYYY"));
           day1Icon.src = "http://openweathermap.org/img/w/" + oneCallData.daily[0].weather[0].icon + ".png";
@@ -152,5 +161,8 @@ let weatherFunction = (searchCity) => {
       console.error(error)
     })
 }
+//---End of Weather Function---//
+//============================//
 
+// Sumbit Button for Search Bar
 submitBtn.addEventListener('click', searchBar);
